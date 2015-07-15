bf685da8709afe37c6f209d76400f71c73d15628
enlizhang <18868816564@163.com>
2015-07-15 14:20:46 +0800

    Initial

---
+++ app/src/main/java/com/netease/easecos/dialog/MyAlertDialog.java
@@ -0,0 +1,8 @@
+package com.netease.easecos.dialog;
+
+/**
+ * 类描述：提示对话框
+ * Created by enlizhang on 2015/7/15.
+ */
+public class MyAlertDialog {
+}
---
+++ app/src/main/java/com/netease/easecos/fragment/BaseFragment.java
@@ -0,0 +1,12 @@
+package com.netease.easecos.fragment;
+
+import android.app.Fragment;
+
+/**
+ * ÀàÃèÊö£ºFragment»ùÀà
+ * Created by enlizhang on 2015/7/15.
+ */
+public class BaseFragment extends Fragment {
+
+
+}
