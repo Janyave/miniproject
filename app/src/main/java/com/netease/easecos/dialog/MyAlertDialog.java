83e9094fc15173b1cd9a99cb1f4687246b023a13
桔子 <hzlengzhaoyang@corp.netease.com>
2015-07-15 16:11:55 +0800

    clear

--- app/src/main/java/com/netease/easecos/dialog/MyAlertDialog.java
+++
@@ -1,8 +0,0 @@
-package com.netease.easecos.dialog;
-
-/**
- * 提示对话框
- * Created by enlizhang on 2015/7/15.
- */
-public class MyAlertDialog {
-}
--- app/src/main/java/com/netease/easecos/dialog/MyProgressDialog.java
+++
@@ -1,8 +0,0 @@
-package com.netease.easecos.dialog;
-
-/**
- * 加载对话框
- * Created by enlizhang on 2015/7/15.
- */
-public class MyProgressDialog {
-}
