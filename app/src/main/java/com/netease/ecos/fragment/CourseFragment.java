ae4ba7ae811d73bbb8c16cea53c55c785e4613c7
linxuanxuan <hzlinxuanxuan@corp.netease.com>
2015-07-23 16:18:03 +0800

    intergrate

--- app/src/main/java/com/netease/ecos/fragment/CourseFragment.java
+++ app/src/main/java/com/netease/ecos/fragment/CourseFragment.java
@@ -1,5 +1,6 @@
 package com.netease.ecos.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -11,15 +12,16 @@ import android.widget.AbsListView;
 import android.widget.Toast;
 
 import com.netease.ecos.R;
+import com.netease.ecos.activity.BuildCourseActivity;
 import com.netease.ecos.adapter.CourseListViewAdapter;
 import com.netease.ecos.views.AnimationHelper;
 import com.netease.ecos.views.FloadingButton;
 import com.netease.ecos.views.XListView;
 
-/***
+/**
  * 教程页面
  */
-public class CourseFragment extends Fragment implements XListView.IXListViewListener{
+public class CourseFragment extends Fragment implements XListView.IXListViewListener {
 
     private View mainView;
     private FloadingButton btn_floading;
@@ -45,11 +47,11 @@ public class CourseFragment extends Fragment implements XListView.IXListViewList
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
-        mainView=inflater.inflate(R.layout.fragment_course,container,false);
+        mainView = inflater.inflate(R.layout.fragment_course, container, false);
 
-        btn_floading=(FloadingButton)mainView.findViewById(R.id.btn_floading);
+        btn_floading = (FloadingButton) mainView.findViewById(R.id.btn_floading);
 
-        lv_course=(XListView)mainView.findViewById(R.id.lv_course);
+        lv_course = (XListView) mainView.findViewById(R.id.lv_course);
         lv_course.setAdapter(new CourseListViewAdapter(getActivity()));
         lv_course.setDividerHeight(2);
         lv_course.initRefleshTime(this.getClass().getSimpleName());
@@ -62,9 +64,11 @@ public class CourseFragment extends Fragment implements XListView.IXListViewList
             int lvIndext = 0; //当前listView显示的首个Item的Index
             String state = "up"; //当前ListView动作状态 up or down
             Boolean isAnim = false; //是否正在动画
+
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
             }
+
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
 
@@ -73,26 +77,26 @@ public class CourseFragment extends Fragment implements XListView.IXListViewList
                 /***当前可见Item的首个Index*/
                 int nowIndext = firstVisibleItem;
                 /***nowIndex大于lvIndex，ListView下滑*/
-                if (nowIndext > lvIndext&&!isAnim) {
+                if (nowIndext > lvIndext && !isAnim) {
                     nowstate = "down";
                     if (!TextUtils.equals(nowstate, state)) {
                         btn_floading.disappear(new AnimationHelper.DoAfterAnimation() {
                             @Override
                             public void doAfterAnimation() {
-                                isAnim=false;
+                                isAnim = false;
                             }
                         });
                         isAnim = true;
                     }
                 }
                 /***nowIndex小于lvIndex，ListView下滑*/
-                if (nowIndext < lvIndext&&!isAnim) {
+                if (nowIndext < lvIndext && !isAnim) {
                     nowstate = "up";
                     if (!TextUtils.equals(nowstate, state)) {
                         btn_floading.appear(new AnimationHelper.DoAfterAnimation() {
                             @Override
                             public void doAfterAnimation() {
-                                isAnim=false;
+                                isAnim = false;
                             }
                         });
                         isAnim = true;
@@ -100,6 +104,14 @@ public class CourseFragment extends Fragment implements XListView.IXListViewList
                 }
                 state = nowstate;
                 lvIndext = nowIndext;
+            }
+        });
+
+        btn_floading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CourseFragment.this.getActivity(), BuildCourseActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -121,7 +133,7 @@ public class CourseFragment extends Fragment implements XListView.IXListViewList
 
     @Override
     public void onLoadMore() {
-        Toast.makeText(getActivity(),"上拉加载",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "上拉加载", Toast.LENGTH_SHORT).show();
 
         //1秒后关闭加载
         Handler handler = new Handler();
