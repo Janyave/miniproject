ca0f464388f677f7895d5c597db4c6745d3af788
linxuanxuan <hzlinxuanxuan@corp.netease.com>
2015-07-24 11:33:23 +0800

    linxuanxuan'work on 7.24

--- app/src/main/java/com/netease/ecos/activity/UploadWorksActivity.java
+++ app/src/main/java/com/netease/ecos/activity/UploadWorksActivity.java
@@ -1,50 +1,51 @@
 package com.netease.ecos.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.netease.ecos.R;
+import com.netease.ecos.adapter.ExhibitListViewAdapter;
+import com.netease.ecos.views.ExtensibleListView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 /**
- * Created by Think on 2015/7/23.
+ * Created by Think on 2015/7/24.
  */
-public class UploadWorkActivity extends Activity {
+public class UploadWorksActivity extends BaseActivity {
     @InjectView(R.id.tv_title)
     TextView titleTxVw;
     @InjectView(R.id.btn_right_action)
     Button rightButton;
     @InjectView(R.id.tv_left)
     TextView backTxVw;
+    @InjectView(R.id.uploadWorksLsVw)
+    ExtensibleListView worksLsVw;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
         setContentView(R.layout.upload_works_layout);
         ButterKnife.inject(this);
         //implementation on the title bar
-        titleTxVw.setVisibility(View.INVISIBLE);
+        titleTxVw.setText("新建作品");
         rightButton.setText("发布");
         rightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO:send the content to the server.
-                UploadWorkActivity.this.finish();
+                UploadWorksActivity.this.finish();
             }
         });
         backTxVw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                UploadWorkActivity.this.finish();
+                UploadWorksActivity.this.finish();
             }
         });
-        getWindow().setSoftInputMode(
-                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        worksLsVw.setAdapter(new ExhibitListViewAdapter(UploadWorksActivity.this));
     }
 }
