4542a6710c274616fff36a3ded522b661f53b3c9
linxuanxuan <hzlinxuanxuan@corp.netease.com>
2015-07-27 13:38:26 +0800

    modify the fragment in the mainActivity

--- app/src/main/java/com/netease/ecos/activity/MainActivity.java
+++ app/src/main/java/com/netease/ecos/activity/MainActivity.java
@@ -24,44 +24,58 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
-public class MainActivity extends BaseActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks{
+public class MainActivity extends BaseActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     @InjectView(R.id.btn_open)
     Button btn_open;
 
     @InjectView(R.id.radio_group)
-    RadioGroup mRadioGroup ;
+    RadioGroup mRadioGroup;
 
     @InjectView(R.id.pager)
-    ViewPager mViewPager ;
+    ViewPager mViewPager;
 
 
-    /*** 教程tab下标 */
+    /**
+     * 教程tab下标
+     */
     public static final int TAB_COURSE_INDEX = 0;
 
-    /*** 社区tab下标 */
-    public static final int TAB_COMMUCITY_INDEX = 1;
+    /**
+     * 分享tab下标
+     */
+    public static final int TAB_DISPLAY_INDEX = 1;
 
-    /*** 交易tab下标 */
-    public static final int TAB_TRANSACTION_INDEX = 2;
+    /**
+     * 活动tab下标
+     */
+    public static final int TAB_COMMUCITY_INDEX = 2;
 
-    /*** 展示tab下标 */
-    public static final int TAB_DISPLAY_INDEX = 3;
+    /**
+     * 招募tab下标
+     */
+    public static final int TAB_TRANSACTION_INDEX = 3;
 
 
-    /***
+    /**
      * mFragments[0]对应{@link CourseFragment}
      * mFragments[1]对应{@link CommunityFragment}
      * mFragments[2]对应{@link TransactionFragment}
      */
     private Fragment mFragments[] = new Fragment[4];
 
-    /*** 当前所处的tab */
+    /**
+     * 当前所处的tab
+     */
     private int mCurrentTab = 0;
-    /*** {@link #mViewPager}适配器 */
+    /**
+     * {@link #mViewPager}适配器
+     */
     private TabFragmentPagerAdapter mPagerAdapter;
 
-    /*** /侧滑栏 */
+    /**
+     * /侧滑栏
+     */
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
     @Override
@@ -80,12 +94,12 @@ public class MainActivity extends BaseActivity implements NavigationDrawerFragme
     }
 
 
-    /***
+    /**
      * 初始化数据
      */
-    public void initData(){
+    public void initData() {
         //初始化侧滑栏
-        mNavigationDrawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
 
         //初始化侧滑栏
         mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
@@ -100,13 +114,10 @@ public class MainActivity extends BaseActivity implements NavigationDrawerFragme
 
     }
 
-    /***
+    /**
      * 初始化视图
      */
-    private void initViews(){
-        //        mRadioGroup = (RadioGroup)findViewById(R.id.radio_group);
-        //        mViewPager = (ViewPager)findViewById(R.id.pager);
-
+    private void initViews() {
         mPagerAdapter = new TabFragmentPagerAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(mPagerAdapter);
         mViewPager.setCurrentItem(mCurrentTab);
@@ -121,76 +132,77 @@ public class MainActivity extends BaseActivity implements NavigationDrawerFragme
         mRadioGroup.setOnCheckedChangeListener(mOnCheckedChangeListener);
 
         //选中第mCurrentTab个按钮
-        ((RadioButton)mRadioGroup.getChildAt(mCurrentTab)).setChecked(true);
+        ((RadioButton) mRadioGroup.getChildAt(mCurrentTab)).setChecked(true);
     }
 
     /**
      * implements NavigationDrawerFragment.NavigationDrawerCallbacks
-     *
+     * <p/>
      * 点击侧滑栏(NavigationDrawerFragment)item时的回掉函数
+     *
      * @param position 点击的item序号，从0开始
      */
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         Log.e("点击侧滑栏", "点击第" + (position + 1) + "栏");
-
         Toast.makeText(this, "点击第" + (position + 1) + "栏", Toast.LENGTH_LONG).show();
     }
 
-    /***
+    /**
      * {@link #mRadioGroup}的按钮监听器
      */
-    private RadioGroup.OnCheckedChangeListener mOnCheckedChangeListener = new RadioGroup.OnCheckedChangeListener(){
+    private RadioGroup.OnCheckedChangeListener mOnCheckedChangeListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
-            switch(checkedId){
+            switch (checkedId) {
                 //点击教程tab
                 case R.id.radio_1:
                     setCurrentTab(TAB_COURSE_INDEX);
                     break;
-                //点击社区tab
+                //点击分享tab
                 case R.id.radio_2:
+                    setCurrentTab(TAB_DISPLAY_INDEX);
+                    break;
+                //点击活动tab
+                case R.id.radio_3:
                     setCurrentTab(TAB_COMMUCITY_INDEX);
                     break;
-                //点击交易tab
-                case R.id.radio_3:
-                    setCurrentTab(TAB_TRANSACTION_INDEX);
-                    break;
-                //点击展示tab
+                //点击招募tab
                 case R.id.radio_4:
-                    setCurrentTab(TAB_DISPLAY_INDEX);
+                    setCurrentTab(TAB_TRANSACTION_INDEX);
                     break;
             }
         }
     };
 
-    /***
+    /**
      * {@link #mViewPager}选中第index个页面
+     *
      * @param index
      */
-    private void setCurrentTab(int index){
-        if(mViewPager != null && (index >= 0 && index <= 3)){
+    private void setCurrentTab(int index) {
+        if (mViewPager != null && (index >= 0 && index <= 3)) {
             mViewPager.setCurrentItem(index);
             mCurrentTab = index;
         }
     }
 
-    /***
+    /**
      * {@link #mViewPager}换页适配器
      */
-    private ViewPager.OnPageChangeListener mOnPageChangeListener = new ViewPager.OnPageChangeListener(){
+    private ViewPager.OnPageChangeListener mOnPageChangeListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrollStateChanged(int status) {
-
         }
+
         @Override
         public void onPageScrolled(int arg0, float arg1, int arg2) {
         }
+
         @Override
         public void onPageSelected(int index) {
             //选中第index个tab按钮，来触发对应tab选中效果(按钮下方会出现指示线)
-            ((RadioButton)mRadioGroup.getChildAt(index)).setChecked(true);
-
+            ((RadioButton) mRadioGroup.getChildAt(index)).setChecked(true);
         }
     };
 
@@ -199,32 +211,30 @@ public class MainActivity extends BaseActivity implements NavigationDrawerFragme
         public TabFragmentPagerAdapter(FragmentManager fm) {
             super(fm);
         }
+
         @Override
         public Fragment getItem(int tabIndex) {
             switch (tabIndex) {
                 //点击教程tab，要显示教程页面
                 case TAB_COURSE_INDEX:
-                    if(mFragments[TAB_COURSE_INDEX]==null)
+                    if (mFragments[TAB_COURSE_INDEX] == null)
                         mFragments[TAB_COURSE_INDEX] = new CourseFragment();
                     return mFragments[TAB_COURSE_INDEX];
-
-                //点击社区tab，要显示社区页面
-                case TAB_COMMUCITY_INDEX:
-                    if(mFragments[TAB_COMMUCITY_INDEX]==null)
-                        mFragments[TAB_COMMUCITY_INDEX] = new CommunityFragment();
-                    return mFragments[TAB_COMMUCITY_INDEX];
-
-                //点击交易tab，要显示交易页面
-                case TAB_TRANSACTION_INDEX:
-                    if(mFragments[TAB_TRANSACTION_INDEX]==null)
-                        mFragments[TAB_TRANSACTION_INDEX] = new TransactionFragment();
-                    return mFragments[TAB_TRANSACTION_INDEX];
-
-                //点击展示tab，要显示交易页面
+                //点击分享tab，要显示分享页面
                 case TAB_DISPLAY_INDEX:
-                    if(mFragments[TAB_DISPLAY_INDEX]==null)
+                    if (mFragments[TAB_DISPLAY_INDEX] == null)
                         mFragments[TAB_DISPLAY_INDEX] = new DisplayFragment();
                     return mFragments[TAB_DISPLAY_INDEX];
+                //点击活动tab，要显示活动页面
+                case TAB_COMMUCITY_INDEX:
+                    if (mFragments[TAB_COMMUCITY_INDEX] == null)
+                        mFragments[TAB_COMMUCITY_INDEX] = new CommunityFragment();
+                    return mFragments[TAB_COMMUCITY_INDEX];
+                //点击招募tab，要显示招募页面
+                case TAB_TRANSACTION_INDEX:
+                    if (mFragments[TAB_TRANSACTION_INDEX] == null)
+                        mFragments[TAB_TRANSACTION_INDEX] = new TransactionFragment();
+                    return mFragments[TAB_TRANSACTION_INDEX];
             }
             return null;
         }
