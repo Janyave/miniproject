e5e185ab56adaff6d2449d5d7c4ed08188f9fcd0
jixinyu <hzjixinyu@corp.netease.com>
2015-08-06 09:41:20 +0800

    choose dialog and ~dialog

--- app/src/main/java/com/netease/ecos/activity/SearchActivity.java
+++ app/src/main/java/com/netease/ecos/activity/SearchActivity.java
@@ -1,13 +1,20 @@
 package com.netease.ecos.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +22,7 @@ import com.netease.ecos.R;
 import com.netease.ecos.adapter.CourseListViewAdapter;
 import com.netease.ecos.adapter.DisplayListViewAdapter;
 import com.netease.ecos.adapter.SearchHistoryAdapter;
+import com.netease.ecos.views.PopupHelper;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -27,12 +35,21 @@ public class SearchActivity extends Activity {
     private static final String TAG = "Ecos---Search";
     public static final String SearchWord = "SearchWord";
 
+    public static final String SEARCH_TYPE="type";
+
+    public static final int TYPE_COURSE=2;
+    public static final int TYPE_SHARE=3;
+
+    private int TYPE=TYPE_COURSE; //default course
+
     @InjectView(R.id.et_search)
     EditText et_search;
     @InjectView(R.id.tv_confirm)
     TextView tv_confirm;
     @InjectView(R.id.lv_searchHistory)
-    ListView lv_searchHistory;
+    ListView lv_searchHistory;  //历史记录
+    @InjectView(R.id.lv_searchList)
+    ListView lv_searchList;   //搜索结果
 
     @InjectView(R.id.ll_searchType)
     LinearLayout ll_searchType;
@@ -40,6 +57,9 @@ public class SearchActivity extends Activity {
     TextView tv_searchType;
     @InjectView(R.id.iv_searchType)
     ImageView iv_searchType;
+
+    PopupWindow courseTypePopupWindow;
+    PopupWindow shareSortTypePopupWindow;
 
     private SearchHistoryAdapter searchHistoryAdapter;
 
@@ -53,19 +73,46 @@ public class SearchActivity extends Activity {
         setContentView(R.layout.activity_search);
         ButterKnife.inject(this);
 
+        try{
+            Bundle bundle=getIntent().getExtras();
+            TYPE=bundle.getInt(SEARCH_TYPE);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        initView();
         initListener();
         initData();
+    }
+
+    private void initView() {
+        shareSortTypePopupWindow= PopupHelper.newShareSortTypePopupWindow(SearchActivity.this);
+        courseTypePopupWindow=PopupHelper.newSixTypePopupWindow(SearchActivity.this);
     }
 
     private void initListener() {
         tv_confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putString(SearchWord, et_search.getText().toString());
-                getIntent().putExtras(bundle);
-                setResult(CourseCategoryActivity.ResultCodeForSearch, getIntent());
-                finish();
+                //TODO  确定事件
+//                Bundle bundle = new Bundle();
+//                bundle.putString(SearchWord, et_search.getText().toString());
+//                getIntent().putExtras(bundle);
+//                setResult(CourseCategoryActivity.ResultCodeForSearch, getIntent());
+//                finish();
+            }
+        });
+
+        et_search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId== EditorInfo.IME_ACTION_DONE ||(event!=null&&event.getKeyCode()== KeyEvent.KEYCODE_ENTER))
+                {
+                    //TODO 软键盘确定事件 搜索事件
+                    //确定后隐藏lv_historyList 显示lv_searchList
+                    return true;
+                }
+                return false;
             }
         });
 
@@ -83,8 +130,39 @@ public class SearchActivity extends Activity {
         ll_searchType.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO
+                if (TYPE==TYPE_COURSE){
+                    if (courseTypePopupWindow.isShowing()){
+                        courseTypePopupWindow.dismiss();
+                    }else {
+                        PopupHelper.showSixTypePopupWindow(courseTypePopupWindow, SearchActivity.this, v, new PopupHelper.IPopupListner() {
+                            @Override
+                            public void clickListner(int type, View v, PopupWindow popupWindow) {
+                                //TODO 选择教程八种类型 type为类型
+                                tv_searchType.setText(((RadioButton) v).getText().toString());
+                                Toast.makeText(SearchActivity.this, "choose " + type, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+                if (TYPE==TYPE_SHARE){
+                    if (shareSortTypePopupWindow.isShowing()){
+                        shareSortTypePopupWindow.dismiss();
+                    }else {
+                        PopupHelper.showSixTypePopupWindow(shareSortTypePopupWindow, SearchActivity.this, v, new PopupHelper.IPopupListner() {
+                            @Override
+                            public void clickListner(int type, View v, PopupWindow popupWindow) {
+                                //TODO 选择分享四种类型 type为类型
+                                tv_searchType.setText(((RadioButton) v).getText().toString());
+                                Toast.makeText(SearchActivity.this, "choose " + type, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
                 Toast.makeText(SearchActivity.this, "choose Dialog", Toast.LENGTH_SHORT).show();
+
+                InputMethodManager inputMethodManager=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(SearchActivity.this.getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+                inputMethodManager.showSoftInput(et_search,InputMethodManager.SHOW_FORCED);
             }
         });
     }
